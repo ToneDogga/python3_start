35182051c0e921699daf99ac05b50ff4d6dc1e7b
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2019-06-23 12:16:22 +0930

    Add files via upload

--- socket_comms_client.py
+++ socket_comms_client.py
@@ -10,18 +10,18 @@ port = 60000                    # Reserve a port for your service.
 s.connect((host, port))
 s.send("Hello server!")
 
-with open('received_file', 'wb') as f:
-    print 'file opened'
+with open("received_file", "wb") as f:
+    print("file opened")
     while True:
-        print('receiving data...')
+        print("receiving data...")
         data = s.recv(1024)
-        print('data=%s', (data))
+        print("data=%s", (data))
         if not data:
             break
         # write data to a file
         f.write(data)
 
 f.close()
-print('Successfully get the file')
+print("Successfully get the file")
 s.close()
-print('connection closed')
+print("connection closed")
--- socket_comms_server.py
+++ socket_comms_server.py
@@ -8,25 +8,25 @@ host = socket.gethostname()     # Get local machine name
 s.bind((host, port))            # Bind to the port
 s.listen(5)                     # Now wait for client connection.
 
-print 'Server listening....'
+print("Server listening....")
 
 while True:
     conn, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
+    print("Got connection from", addr)
     data = conn.recv(1024)
-    print('Server received', repr(data))
+    print("Server received", repr(data))
 
-    filename='mytext.txt'
-    f = open(filename,'rb')
+    filename="mytext.txt"
+    f = open(filename,"rb")
     l = f.read(1024)
     while (l):
        conn.send(l)
-       print('Sent ',repr(l))
+       print("Sent ",repr(l))
        l = f.read(1024)
     f.close()
 
-    print('Done sending')
-    conn.send('Thank you for connecting')
+    print("Done sending")
+    conn.send("Thank you for connecting")
     conn.close()
 
 
