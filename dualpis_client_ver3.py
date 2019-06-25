05792f2ee39d2d07b030a50cd173313b89398743
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2019-06-25 18:59:20 +0930

    Add files via upload

--- dualpis_client_ver3.py
+++ dualpis_client_ver3.py
@@ -3,10 +3,11 @@
 
 import socket  # Import socket module
 import brickpi3
+import time
 
 
 BP=brickpi3.BrickPi3()
-
+BP.offset_motor_encoder(BP.PORT_A,BP.get_motor_encoder(BP.PORT_A))
 
 s = socket.socket()             # Create a socket object
 host= socket.gethostbyname("192.168.0.110")
@@ -17,29 +18,40 @@ print("host=",host)
 s.connect((host, port))
 s.send(b'Hello server!')
 
-#with open("received_file.txt", "wb") as f:
-#    print("file opened")
-sdata=""
+
+motor_pos=0
+BP.set_motor_position(BP.PORT_A,motor_pos)
+old_motor_pos=0
+
+
 try:
     while True:
         #print("receiving data...")
-        olddata=sdata
-
+        #time.sleep(0.02)
         #data = s.recv(1024)
-        data=s.recv(6)
-        sdata=data.decode('utf-8')
-        if sdata != olddata:
-           # print(sdata)
-            motor_pos=int(sdata)
-           # print("motor pos=",motor_pos)
-            BP.set_motor_position(BP.PORT_A,motor_pos)
+        error_flag=False
+        data=s.recv(8)
         if not data:
             break
-        # write data to a file
- #       f.write(data)
 
-#f.close()
-#print("Successfully got the file")
+        sdata=data.decode('utf-8')
+        
+        try:
+            str(sdata)
+            try:
+                int(sdata)
+                motor_pos=int(sdata)
+                error_flag=False
+            except ValueError:
+                error_flag=True
+                motor_pos=0
+        except ValueError:
+            error=True
+            motor_pos=0
+        if not error_flag and motor_pos!=old_motor_pos:
+            #print(motor_pos)
+            BP.set_motor_position(BP.PORT_A,motor_pos)
+
 except KeyboardInterrupt:
     s.close()
 print("connection closed")
