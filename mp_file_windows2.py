1ab707fe9c155d800f9b0723f35c1c66b2b73d18
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2019-07-31 11:29:23 +0930

    Add files via upload

--- mp_file_windows2.py
+++ mp_file_windows2.py
@@ -3,7 +3,7 @@ from __future__ import division
 
 #from multiprocessing.dummy import Pool, TimeoutError, Process, Lock, freeze_support, Queue, Lock, Manager
 #from multiprocessing import TimeoutError, Pool, Process, Lock, freeze_support, Queue, Lock, Manager
-from multiprocessing.dummy import Pool, Process, Lock, freeze_support, Queue, Lock, Manager
+from multiprocessing import Pool, Process, Lock, freeze_support, Queue, Lock, Manager
 
 #from  multiprocessing import Pool, Queue
 from os import getpid
@@ -21,33 +21,57 @@ from pathlib import Path
 EOL='\n'
 FILENAME="testfile1.csv"
 import multiprocessing 
-import time
+from timeit import default_timer as timer
 
 fn = 'c:/temp/temp.txt'
 
 
 def listener(q):    #,l_lock):
     '''listens for messages on the q, writes to file. '''
-
+    print("Queue listener started on:",os.getpid())
    # l_lock.acquire()
-    f=open(FILENAME,"a")   #,buffering=0)
+    f=open(FILENAME,"a")   #,buffering=8096)
     #f.flush()
     while True:
-        if q.full():
-            print("q full")
-        m = q.get()    #timeout=20)
-        if m == 'kill':
-            #f.write('killed')
+       # if q.full():
+       #     print("q full")
+       #     break
+        m = q.get()    #timeout=20)  get_nowait
+        if m == "kill":
+            print("trying to kill listener process.  Flushing q.  qsize=",q.qsize())
+            #f.write("killed\n")
+            while not q.empty():
+                m=q.get()
+                f.write(m)            
+                f.flush()
             break
- 
-        f.write(m)
-        f.flush()
+        else: 
+            f.write(m)  #+":"+q.qsize()+"\n")
+          #  f.write(str(q.qsize())+'\n')
+            f.flush()
        
+  #  q.close()
     f.close()
+    print("Queue listener closed on:",os.getpid())
    # l_lock.release()
+    return(True)
 
-
-
+def add_to_queue(msg,q):
+    temp_buffer=[]
+      #  If queue is full, put the message in a temporary buffer.
+      #  If the queue is not full, adding the message to the queue.
+      #  If the buffer is not empty and that the message queue is not full,
+      #  putting back messages from the buffer to the queue.
+  #  print(q.qsize())    
+    if q.full():
+        temp_buffer.append(msg)
+    else:
+        q.put(msg)
+     #   q.put(str(q.qsize())+'\n')
+        if len(temp_buffer) > 0:
+            add_to_queue(temp_buffer.pop())
+    #history.append(q.qsize)
+    return   #(history)
 
 def generate_payoff_environment_1d_mp(astart_val,asize_of_env,q):    #l_lock  
     rowno=0
@@ -62,7 +86,8 @@ def generate_payoff_environment_1d_mp(astart_val,asize_of_env,q):    #l_lock
     for a in range(astart_val,astart_val+asize_of_env):
         payoff=-10*math.sin(a/44)*12*math.cos(a/33)*1000/(a+1)
         pline=(str(rowno)+","+str(a)+","+str(payoff)+"\n")
-        q.put(pline)
+        add_to_queue(pline,q)
+      #  q.put(pline)
       #  if q.full():
       #      print("q full")
         rowno+=1
@@ -116,8 +141,12 @@ if __name__ == '__main__':
     q = manager.Queue()    
    # pool = mp.Pool(mp.cpu_count() + 2)
 
+
+    cpus = multiprocessing.cpu_count()
     #put listener to work first
    # watcher = pool.apply_async(listener, (q,))
+   # with Pool(processes=cpus) as pool:  # processes=cpus
+   #     watcher = pool.apply_async(listener, args=(q, ))
 
 
     windows=platform.system().lower()[:7]
@@ -127,10 +156,9 @@ if __name__ == '__main__':
     else:
         EOL='\n'
 
-    cpus = multiprocessing.cpu_count()
  #   cpus = cpu_count()
     print("cpus=",cpus)
-    cpus=cpus-2    # allow one for existing processes and one for watcher
+   # cpus=cpus-2    # allow one for existing processes and one for watcher
 
  #   my_file = Path("/home/pi/Python_Lego_projects/testfile1.csv")
     my_file = Path("/home/pi/Python_Lego_projects/testfile1.csv")
@@ -145,33 +173,50 @@ if __name__ == '__main__':
   
     #clock_start=time.process_time()
 
+   # with Pool(processes=1) as pool2:  # processes=cpus-1
+   #     watcher = pool2.apply_async(listener, args=(q, ))
+
+    clock_start=timer() #time.process_time()
     #lock=Lock()
 
-    chunk=int(row_numbers/cpus)
-    remainder=row_numbers%cpus
+    chunk=int(row_numbers/(cpus-1))   # less one cpu for listener
+    remainder=row_numbers%(cpus-1)
     print("rownumbers=",row_numbers," chunk size=",chunk," remainder=",remainder)
-    with Pool(processes=cpus) as pool:  # processes=cpus
-        watcher = pool.apply_async(listener, (q, ))
-        
+
+    with Pool(processes=cpus) as pool:  # processes=cpus-1
+
+      #  watcher = pool.apply_async(listener, args=(q, ))
+        watcher = pool.apply_async(listener, args=(q, ))
+
+
         if remainder!=0:
-            multiple_results = [pool.apply_async(generate_payoff_environment_1d_mp,(0,remainder,q, ))]
+            multiple_results = [pool.apply_async(generate_payoff_environment_1d_mp,args=(0,remainder,q, ))]
         else:
             multiple_results=[]
-        for i in range(0,cpus):
-            multiple_results.append(pool.apply_async(generate_payoff_environment_1d_mp,(i*chunk+remainder,chunk,q, )))
+        for i in range(0,cpus-1):
+            multiple_results.append(pool.apply_async(generate_payoff_environment_1d_mp,args=(i*chunk+remainder,chunk,q, )))
 
        # print("mr=",len(multiple_results))
         for res in multiple_results:
             result=res.get(timeout=None)       
             res.wait()
 
+        sleep(5)
+        print("Generate payoff results finished")
+
+        print("killing listener")
+        q.put("kill")
+        result=watcher.get(timeout=None) 
+        watcher.wait()
+
+        
     # Waits a bit for the child processes to do some work
     # because when the parent exits, childs are terminated.
  
-    print("Generate payoff results finished")
-    q.put("kill")
-    pool.close()
-    pool.join()
+   
+    
+   # pool.close()
+   # pool.join()
 
 
 
@@ -182,7 +227,27 @@ if __name__ == '__main__':
 
 
 
-   # sleep(5)
+    #sleep(5)
+   
+   # sleep(1)
+ 
+    #result=watcher.get(timeout=5)
+   # pool2.close()
+  #  result=watcher.get(timeout=40) 
+   # sleep(2)
+    print("try to close pool")
+    pool.close()
+    print("pool closed.  trying to join() pool")
+    pool.join()
+    print("pool join() complete")
+   # sleep(1)
+
+
+    clock_end=timer()   #time.process_time()
+    duration_clock=clock_end-clock_start
+    print("gen payoff file windows multi processor rows=",row_numbers," Time:", duration_clock," secs.")
+
+   
     print("count file rows=",count_file_rows(FILENAME))
     print(FILENAME," #=",hash_a_file(FILENAME))      
 
