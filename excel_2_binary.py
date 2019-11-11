38a373782ae082c3e88cb889ed43ad62778023d4
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2019-11-11 20:14:13 +1030

    Add files via upload

--- excel_2_binary.py
+++ excel_2_binary.py
@@ -5,66 +5,116 @@ import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
 from collections import defaultdict
 
-#d = defaultdict(LabelEncoder)
-#d = defaultdict(LabelEncoder)
-
 mlb = MultiLabelBinarizer()
-
 lb = preprocessing.LabelBinarizer()
-#lb.fit([1, 2, 6, 4, 2])
 
-##print("1",lb.classes_)
-##
-##print("2",lb.transform([1, 4, 2, 6]))
-##
-##print("2a",lb.fit_transform(['yes', 'no', 'no', 'yes']))
-##
-##
-##lb.fit(np.array([[0, 1, 1], [1, 0, 0]]))
-##
-##print("3",lb.classes_)
-##
-##
-##print("4",lb.transform([0, 1, 2, 1]))
-##
-##print("5",lb.fit_transform(['SJ300',"aj300","SPJ300"]))
-##
-##print("6",lb.transform(["rj300",'SJ300',"WS300"]))
-
-#df=pd.read_excel("testdata.xlsx", "Sheet1",dtype={"cat":np.int32,"productgroup":np.int32,"qty":np.int32},convert_float=True).head(int(sys.argv[2]))
-#df=pd.read_excel("testdata.xlsx",convert_float=True)
+def read_excel(filename,rows):
+    xls = pd.ExcelFile(filename)    #'salestransslice1.xlsx')
+    df = xls.parse(xls.sheet_names[0]).head(rows)
+    #xd=df.to_dict()
+    print(df)
+    return df
 
 
-xls = pd.ExcelFile('salestransslice1.xlsx')
-df = xls.parse(xls.sheet_names[0])[0:5]
-#xd=df.to_dict()
-print(df)
-colnames=df.columns
-ef=np.array(dtype=str)
-#ef=pd.DataFrame(columns=colnames)
-print("ef=\n",ef)
+def encode(df):
+    colnames=df.columns
+    coldict=defaultdict(int)
 
-print(colnames)
-#df.iloc[0]=colnames
-i=0
-for col in iter(colnames):
-    #df2=df[col]
-    #df=df["code"]
-    #print("i=",i,"col=",col,"df=\n",df)
-    #products=["SJ300","RJ300","10","5","6"]
-    #print("start products=",products)
-    prodb=df[col].values.astype(str)
-    print(colnames[i],"=\n",prodb)
-    #prodbin=mlb.fit_transform(prodb)   #products)
-    #dtype=dict(names=[col,formats=str)
-    ef=hstack(ef,lb.fit_transform(prodb))
-    i+=1
-    #print("pb=\n",prodbin,"\nstr=\n",sprod)
-   # print("prodbin.shape=",prodbin.shape)
-   # print("pb=\n",prodbin)
+ #   print(colnames)
+    i=0
+    for col in iter(colnames):
+        prodb=df[col].values.astype(str)
+        bbin=lb.fit_transform(prodb)
+        if i==0:
+            start=0
+            ef=bbin
+        else:
+            start=finish
+            ef=np.hstack((ef,bbin))
+
+        finish=ef.shape[1]
+        colwidth=finish-start
+        coldict[i]={colnames[i]:colwidth}
+       # print("colwidth=",colwidth,"i=",i,"col=",col,"ef=\n",ef)
+        print(coldict[i],"=\n",prodb,"binary=",bbin)
+
+        i+=1
+    return ef,coldict
 
 
-print("ef=\n",ef)
+def create_conditions(npbin):
+  #  length=npbin.shape[1]
+  #  npbin=npbin.astype(str)
+   # print("npbin=",npbin)
+ #   k=np.array(list(map(''.join, npbin.astype(str))))
+    j=pd.DataFrame(columns=["condition","message"])
+    j["condition"]=list(map(''.join, npbin.astype(str)))
+    return j
+
+
+
+def decode(df,coldict):
+   # colnames=coldict[1][0]
+   # print("keys=",coldict.keys())
+   # print("values=",coldict.values())
+   # print("items=",coldict.items())
+
+    values=list(coldict.values())
+    keys=list(coldict.keys())
+    
+
+    #col_len=len(coldict.keys())
+    #print("col len",col_len)
+    #colnames=list(coldict.values())
+  #  print(colnames)
+
+    print("values",values,"keys",keys)
+    #print("colnames:",colnames[2][1])
+
+    i=0
+    for col in iter(coldict):
+        print(coldict.get[col])
+        
+##        prodb=df[col].values.astype(str)
+##        print(colnames[i],"=\n",prodb)
+##        if i==0:
+##            start=0
+##            ef=lb.fit_transform(prodb)
+##        else:
+##            start=finish
+##            newproducts=lb.inverse_transform(prodbin)
+##            #ef=np.hstack((ef,lb.fit_transform(prodb)))
+##
+##        finish=ef.shape[1]
+##        colwidth=finish-start
+##        coldict[i]={colnames[i]:colwidth}
+##        print("colwidth=",colwidth,"i=",i,"col=",col,"ef=\n",ef)
+##        i+=1
+        #print("pb=\n",prodbin,"\nstr=\n",sprod)
+       # print("prodbin.shape=",prodbin.shape)
+       # print("pb=\n",prodbin)
+
+
+    print("ef=\n",ef)
+    print("coldict=",coldict)
+    return ff
+
+pd.set_option('max_colwidth', 100)
+#pd.options.display.width = 0
+pd.options.display.max_rows = 999
+df=read_excel("salestransslice1.xlsx",5)
+bin_encode,column_dict=encode(df)
+print("bin encode shape=",bin_encode.shape)
+print("bin encode=\n",bin_encode)
+print("coldict=\n",column_dict)
+gf=create_conditions(bin_encode)
+print("conditions dataframe=\n",gf.to_string())
+print("shape=",gf.shape)
+ff=decode(bin_encode,column_dict)
+print("ff shape=",ff.shape)
+
+
+
 #d=d.T
 #print("d.T=\n",d)
 #array = np.fromiter(d.items(), dtype=int, count=len(d))
