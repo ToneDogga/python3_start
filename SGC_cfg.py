94697cee022fe8a0b58967871076b669eae9f315
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2019-11-24 16:34:19 +1030

    Add files via upload

--- SGC_cfg.py
+++ SGC_cfg.py
@@ -1,5 +1,5 @@
 filename="salestransslice1.xlsx"
-importrows=18
-excludecols=[2,3,4,5,10,11,13]
-bins=[1,2,4,8,16,40,80,2000]
+importrows=13
+excludecols=[2,3,4,5,10,11,13]  #[2,3,4,5,10,11,13]
+bins=[0,1,2,4,8,16,40,80,2000]
 ycolno=9
