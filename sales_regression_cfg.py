0293a779dbe696d4c1c88ace3daf11df6ea0603b
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2019-12-09 20:04:43 +1030

    Add files via upload

--- sales_regression_cfg.py
+++ sales_regression_cfg.py
@@ -1,4 +1,12 @@
-infilename="FLNOR-raw.xlsx"
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+
+#infilename="FLFUL_FLPAS_FLFRE-raw.xlsx"
+#infilename="FLNOR_FLBRI-raw.xlsx"
+#infilename="FLNOR-raw.xlsx"
+infilename="NAT-raw.xlsx"
+
+
 importrows=-1  # all
 
 code_encode_save="customer_encode_save.pkl"
@@ -15,9 +23,23 @@ SGDC_save="SGDClassifier.pkl"
 RFC_save="RFClassifier.pkl"
 
 outfile="RegressionResults.txt"
+outfile_predict="PredictResults.txt"
+
 outxlsfile="SalesPredictResults.xlsx"
 
+datasetpluspredict="datasetpluspredict.csv"
+datasetworking="datasetworking.csv"
 
+
+##########################################################
+
+#customer_codes=["FLNOR","FLBRI"]
+
+predict_start_date=dt.datetime.now
+one_year_ago = (dt.datetime.now()+relativedelta(years=-1)).strftime('%Y/%m/%d')
+
+
+#####################################################
 # binning.  no columns or rows are excluded
 #ycolno=1
 #ybins=[0,8,16,24,32,40,56,80,2000]
@@ -39,6 +61,7 @@ featureorder_c=["code_encode","prod_encode","qty","productgroup","date","date_en
 #################################3
 #include_productgroup=[10,11,12,13,14,15,16]
 
+#RF_estimators=4000  # regression
 RF_estimators=1000  # regression
 RFC_estimators=100   # classification
 mintransactions=16
