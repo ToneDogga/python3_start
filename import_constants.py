b5d7bfb4c6a99ce7fb90e91a0d0632249df639bc
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2020-04-03 14:50:06 +1030

    Add files via upload

--- import_constants.py
+++ import_constants.py
@@ -35,14 +35,14 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
 
 
 
-predict_ahead_steps=220
+predict_ahead_steps=320
 epochs_cnn=1
-epochs_wavenet=20
-no_of_batches=10000   #1       # rotate the weeks forward in the batch by one week each time to maintain the integrity of the series, just change its starting point
-batch_length=16  # one week=5 days   #4   #731   #731  #365  3 years of days  1096
+epochs_wavenet=100
+no_of_batches=60000   #1       # rotate the weeks forward in the batch by one week each time to maintain the integrity of the series, just change its starting point
+batch_length=16 # 16  # one week=5 days   #4   #731   #731  #365  3 years of days  1096
 y_length=1
-neurons=800
-start_point=100
+neurons=514
+start_point=150
 #predict_start_point=20
  #   plot_y_extra=1000   # extra y to graph plot 
  #   mat_length=20
@@ -61,7 +61,7 @@ test_percent=0.1
 filename="NAT-raw310120all.xlsx"
    #     filename="allsalestrans020218-190320.xlsx"   
 
-mats=[16]    # series moving average window periods for each data column to add to series table
+mats=[65]    # 22 work days is approx one month 16 series moving average window periods for each data column to add to series table
 
 
     
@@ -95,7 +95,7 @@ def load_data(mats,filename):    #,col_name_list,window_size):   #,mask_text):  
  #   mask = mask.replace('"','').strip()    
  #   print("mask=",mask)
     
-  #  mask=((df['code']=='FLPAS') & (df['product']=='SJ300'))
+    mask=((df['code']=='FLPAS') & (df['product']=='SJ300'))
   #  mask=((df['productgroup']>=10) & (df['productgroup']<=14))
  #   mask=(df['product']=='SJ300')
   #  mask=(df['code']=='FLPAS')
@@ -104,10 +104,10 @@ def load_data(mats,filename):    #,col_name_list,window_size):   #,mask_text):  
  #   mask=((df['productgroup']>=10) & (df['productgroup']<=14))
  #   mask=bool(mask_str)   #((df['productgroup']>=10) & (df['productgroup']<=14))"
 
- #   mask=(df['productgroup']==10)
+  #  mask=(df['productgroup']==11)
  #   mask=((df['code']=='FLPAS') & (df['product']=="SJ300"))
   
-    mask=((df['code']=='FLPAS') & ((df['product']=="CAR280") | (df['product']=="SJ300")))
+ #   mask=((df['code']=='FLPAS') & ((df['product']=="CAR280") | (df['product']=="SJ300")))
   #  mask=((df['code']=='FLPAS') & (df['productgroup']==10))  # & ((df['product']=='SJ300') | (df['product']=='AJ300')))
  #   mask=((df['code']=='FLPAS') & ((df['product']=='SJ300') | (df['product']=='AJ300') | (df['product']=='TS300')))
 
@@ -127,7 +127,7 @@ def load_data(mats,filename):    #,col_name_list,window_size):   #,mask_text):  
    # df[mask]=df[mask].astype('category')
 
 #    table = pd.pivot_table(df[mask], values='qty', index=['productgroup'],columns=['period'], aggfunc=np.sum, margins=False,dropna=False,observed=False, fill_value=0).T  #observed=True
-    table = pd.pivot_table(df[mask], values='qty', index=['productgroup'],columns=['period'], aggfunc=np.sum, margins=False,dropna=False,observed=False, fill_value=0).T  #observed=True
+    table = pd.pivot_table(df[mask], values='qty', index=['code','product'],columns=['period'], aggfunc=np.sum, margins=False,dropna=False,observed=False, fill_value=0).T  #observed=True
 
     colnames=list(table.columns)
     print("colnames=\n",colnames)
