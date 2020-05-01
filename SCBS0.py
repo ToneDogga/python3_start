a808c69064eb5e81c37c83cf24063a03fa960849
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2020-05-01 11:41:59 +0930

    Add files via upload

--- SCBS0.py
+++ SCBS0.py
@@ -8,76 +8,83 @@ Created on Thu Apr 30 13:58:04 2020
 
 
 import numpy as np
-predict_ahead_steps=440
+from datetime import datetime
+import os
+
+
+def log_dir(prefix=""):
+    now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    root_logdir = "./SCBS_outputs"
+    if prefix:
+        prefix += "-"
+    name = prefix + "run-" + now
+    return "{}/{}/".format(root_logdir, name)
+
+
+
+
+class c(object):
+    pass
+  
+c.predict_ahead_steps=44
 
  #   epochs_cnn=1
-epochs_wavenet=20
-no_of_batches=100000   #1       # rotate the weeks forward in the batch by one week each time to maintain the integrity of the series, just change its starting point
-batch_length=16   #16 # 16  # one week=5 days   #4   #731   #731  #365  3 years of days  1096
+c.epochs_wavenet=5
+c.no_of_batches=80000   #1       # rotate the weeks forward in the batch by one week each time to maintain the integrity of the series, just change its starting point
+c.batch_length=16   #16 # 16  # one week=5 days   #4   #731   #731  #365  3 years of days  1096
 #    y_length=1
-neurons=1600  #1000-2000
- 
-pred_error_sample_size=40
+c.neurons=1000  #1000-2000
+c.dropout_rate=0.4
 
-patience=5   #5
+c.pred_error_sample_size=100
 
-# dictionary mat type code :   aggsum field, name, color
-   # mat_type_dict=dict({"u":["qty","units","b-"]
-                  #  "d":["salesval","dollars","r-"],
-                  #  "m":["margin","margin","m."]
-#                   })
+c.patience=5   #5
    
-mats=[28]   #omving average window periods for each data column to add to series table
-start_point=np.max(mats)+15  # we need to have exactly a multiple of 365 days on the start point to get the zseasonality right  #batch_length+1   #np.max(mats) #+1
-mat_types=["u"]  #,"d","m"]
+c.mats=[28]   #omving average window periods for each data column to add to series table
+c.start_point=np.max(c.mats)+c.batch_length  # we need to have exactly a multiple of 365 days on the start point to get the zseasonality right  #batch_length+1   #np.max(mats) #+1
+c.mat_types=["u"]  #,"d","m"]
    
-units_per_ctn=8
+c.units_per_ctn=8
    
 # train validate test split 
-train_percent=0.7
-validate_percent=0.2
-test_percent=0.1
+c.train_percent=0.7
+c.validate_percent=0.2
+c.test_percent=0.1
 
-filenames=["allsalestrans2020.xlsx","allsalestrans2018.xlsx"]
+c.filenames=["allsalestrans2020.xlsx","allsalestrans2018.xlsx"]
 
-queryfilename="queryfile.xlsx"
-
-
+c.queryfilename="queryfile.xlsx"
 
 
-   # filename="NAT-raw310120_no_shop_WW_Coles.xlsx"
-#filename="NAT-raw240420_no_shop_WW_Coles.xlsx"
- 
-   #     filename="allsalestrans020218-190320.xlsx"   
-
-#index_code=['code','productgroup'] 
- #   index_code=['code','productgroup'] 
-   # index_code=['product','code'] 
-  
-#index_code=['code','product'] 
-   # index_code=['code']
-   # index_code=['product']  
-   # index_code=['productgroup'] 
-   # index_code=['code']
-   # index_code=['code','productgroup','product']
-###############################################33
-  #  you also need to change the mask itself which is in the load data function
-#################################################    
+c.required_starting_length=(365*2)+np.max(c.mats)+c.batch_length   # 2 years plus the MAT data lost at the start + batchlength
 
 
- #   mats=[30,90]   #omving average window periods for each data column to add to series table
- #   mat_types=["d"]  #,"d","m"]
-  
-
-#print("\nexcel input data filename='",filename,"'\n")
-#print("excel query fields=",index_code)
-#print("moving average days",mats)
-#print("start point",start_point)
-#print("predict ahead steps=",predict_ahead_steps,"\n")
-required_starting_length=731+np.max(mats)+batch_length   # 2 years plus the MAT data lost at the start + batchlength
 
 
 ########################################################################    
+
+c.output_dir = log_dir("SCBS")
+os.makedirs(c.output_dir, exist_ok=True)
+
+
+c.images_path = os.path.join(c.output_dir, "images/")
+os.makedirs(c.images_path, exist_ok=True)
+    
+
+
+
+#file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+#m, n = X_train.shape
+
+
+#checkpoint_path = "/tmp/my_deep_mnist_model.ckpt"
+#checkpoint_epoch_path = checkpoint_path + ".epoch"
+#final_model_path = "./my_deep_mnist_model"
+
+#########################################################
+
+
 
 #    load the sales_trans.xls file
 # load the query.xls file
@@ -88,24 +95,35 @@ required_starting_length=731+np.max(mats)+batch_length   # 2 years plus the MAT 
 # pipeline
 
 
+import SCBS1_excel_import_v1_02 as scbs1 
+import SCBS2_batches_v1_00 as scbs2 
+import SCBS3_model_v1_00 as scbs3 
+import SCBS4_predict_v1_00 as scbs4 
 
-loaddata = __import__('SCBS_excel_import_v1-02')
-batch = __import__('SCBS_batches_v1-00')
-learn = __import__('SCBS_model_v1-00')
-predict = __import__('SCBS_predict_v1-00')
-
-
-from sklearn.pipeline import Pipeline
-
-series_prediction_model = Pipeline([
-        ("excel_load", loaddata.main()),
-        ("batching", batch.main()),
-        ("learn", learn.main()),
-        ("predict", predict.main()),
-    ])
+scbs1.main(c)
+scbs2.main(c)
+scbs3.main(c)
+scbs4.main(c)
 
 
-sales_prediction_model.predict()
+
+# xloaddata = __import__('SCBS1_excel_import_v1-02')
+# xbatch = __import__('SCBS2_batches_v1-00')
+# xlearn = __import__('SCBS3_model_v1-00')
+# xpredict = __import__('SCBS4_predict_v1-00')
+
+
+# from sklearn.pipeline import Pipeline
+
+# series_prediction_model = Pipeline([
+#          ("excel_load", scbs1),
+#          ("batching", scbs2),
+#          ("learn", scbs3),
+#          ("predict", scbs4),
+#      ])
+
+
+# sales_prediction_model.predict([])
 
 #exec(open("test2.py").read())
 
