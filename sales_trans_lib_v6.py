38bc4b776d7b9350ddad6332440f75f8998d092f
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2020-06-30 12:46:08 +0930

    Add files via upload

--- sales_trans_lib_v6.py
+++ sales_trans_lib_v6.py
@@ -549,9 +549,9 @@ class salestrans:
        
      
     def plot_learning_curves(self,loss, val_loss,epochs,title):
-        if ((np.min(loss)<=0) or (np.max(loss)==np.inf)):
+        if ((np.min(loss)<=0) or (np.max(loss)==np.inf) or (isnan(loss))):
             return
-        if ((np.min(val_loss)<=0) or (np.max(val_loss)==np.inf)):
+        if ((np.min(val_loss)<=0) or (np.max(val_loss)==np.inf) or (isnan(val_loss))):
             return
         if np.min(loss)>10:
             lift=10
