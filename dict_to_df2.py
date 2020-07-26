950b0cb545d6901caaa3517b5af1d0a89cd66dbc
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2020-07-26 18:27:00 +0930

    Add files via upload

--- dict_to_df2.py
+++ dict_to_df2.py
@@ -8,138 +8,239 @@ Created on Sat Jul 25 10:38:28 2020
 from collections import namedtuple
 #from collections import defaultdict
 import pandas as pd
+import pickle
+import matplotlib as mpl
+from matplotlib.pyplot import plot, draw, ion, show
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 
 
-chart_names=["chart1","cr2","ct3"]
-column_names=["col1","col2","col3"]
+class stack:
+    def __init__(self):   
+        self.stack_df_savename="stack_df.pkl"
+        self.stack_plotp_savename="stack_plotp.pkl"
+        self.__version__="0.1.0"
+
+        with open(self.stack_df_savename, 'wb') as f:        
+            pickle.dump([], f,protocol=-1)   
+        with open(self.stack_plotp_savename, 'wb') as f:        
+            pickle.dump([], f,protocol=-1)   
+           
+        return
+    
+ 
+    def push_stack(self,stack_df_element,stack_plotp_element):
+        with open(self.stack_df_savename,"rb") as f:
+            stack_df=pickle.load(f)
+        stack_df.append(stack_df_element)   
+        with open(self.stack_plotp_savename,"rb") as f:
+            stack_plotp=pickle.load(f)
+        stack_plotp.append(stack_plotp_element)   
+
+        with open(self.stack_df_savename, 'wb') as f:        
+            pickle.dump(stack_df, f,protocol=-1)
+        with open(self.stack_plotp_savename, 'wb') as f:        
+            pickle.dump(stack_plotp, f,protocol=-1)
+    
+        return len(stack_df)    
+    
+    
+    def stack_len(self):
+    #     if os.path.isfile('./stack.pkl'):
+        with open(self.stack_df_savename,"rb") as f:
+            stack_df=pickle.load(f)
+        return len(stack_df)   
+    
+    
+    
+    def pop_all_plotps_stack(self):
+        with open(self.stack_plotp_savename,"rb") as f:
+            stack_plotp=pickle.load(f)
+        with open(self.stack_plotp_savename, 'wb') as f:        
+            pickle.dump([], f,protocol=-1)
+        return stack_plotp  
+    
+    
+    def pop_all_dfs_stack(self):
+        with open(self.stack_df_savename,"rb") as f:
+            stack=pickle.load(f)
+        with open(self.stack_df_savename, 'wb') as f:        
+            pickle.dump([], f,protocol=-1)   
+        return stack    
+
+
+
+#################################################3
+
+
+s=stack()
+
+chart_names=["chart1","chart2","chart3_three"]
+with open("measures_savename.pkl", 'rb') as f:
+    column_names = pickle.load(f)
+
+scan_dict_savename="scan_dict.pkl"
+#column_names=["col1","col2","col3"]
+with open(scan_dict_savename, 'rb') as g:
+    scan_data_dict = pickle.load(g)
+
+#print(b)
+#print("\n\n")
+# latest_date=scan_data_dict['final_df'].index[-1]
+# print("\nBeerenberg scan data at:",latest_date)
+# print("+++++++++++++++++++++==================\n")
+
+
+df=scan_data_dict['final_df']
+print(df)
 
 
 #chart = namedtuple("chart", ["name", "kind","stacked","second_y"])
-data_column = namedtuple("data_column", ["active","name", "kind","colour","stacked","second_y"])
+data_column = namedtuple("data_column", ["active","title","y_title", "kind","style","colour","stacked","secondary_y"])
 
-
-
-
-
-# 
-
-#  chart_dict is a dictionary of all the charts created plus the report_type_dict to decode it
-# at the end it is picked so it can be loaded
-
-chart_elements_dict={column_names[0]:data_column(True,column_names[0],"kind","c1",False,False),
-            column_names[1]:data_column(False,column_names[1],"kind2","c2",False,False),
-            column_names[2]:data_column(False,column_names[2],"kind3","c3",False,False)}
-
-#print("\nce=",chart_elements_dict)
-
-no_of_charts=5
-
-#chart_dict={0:chart_elements_dict[0]}
-# fill out the entire charts with True for all columns 
-charts={chart_name:{(chart_name,chart_element):chart_elements_dict[chart_element] for chart_element in chart_elements_dict.keys()} for chart_name in chart_names}
-#chart_dict = defaultdict(lambda x: chart_elements_dict[x])
-
-
-#print("\ncd=",charts)
+chart_elements_dict={column_names[m]:data_column(True,column_names[m],column_names[m][:20],"bar","r-","blue",False,False) for m in range(len(column_names))}
+charts={(chart_name,chart_element):chart_elements_dict[chart_element] for chart_name in chart_names for chart_element in chart_elements_dict.keys()}
 #####################################33
 #  Set values
-charts['ct3'][('ct3','col1')]=data_column(False,'testx1','line','blue',True,False)
-charts['ct3'][('ct3','col2')]=data_column(False,'testx2','line','blue',True,False)
-charts['ct3'][('ct3','col3')]=data_column(False,'testx3','line','blue',True,False)
-charts['cr2'][('cr2','col1')]=data_column(False,'testx4','line','blue',True,False)
-charts['cr2'][('cr2','col2')]=data_column(False,'testx5','line','blue',True,False)
-charts['cr2'][('cr2','col3')]=data_column(False,'testx6','line','blue',True,False)
-#charts[('ct3','col3')]=data_column(False,'testx','line','blue',True,False)
+charts[('chart1','Units (000)')]=data_column(True,'terstx1',"y_title",'line',"g:","",False,False)
+charts[('chart1','Dollars (000)')]=data_column(True,'testxsdfgsd1',"yg_title",'bar',"red","red",True,True)
+#charts[('chart1','col3')]=data_column(True,'tesgsd1',"yfg_title",'line',"b-","",True,False)
+#charts[('chart1','col4')]=data_column(True,'tfffesgsd1',"yfffg_title",'line',"k-","",False,False)
 
-#print("\ncd2=",charts)
+# charts['ct3'][('ct3','col2')]=data_column(False,'testx2','line',"ls",'blue',True,False)
+# charts['ct3'][('ct3','col3')]=data_column(False,'testx3','line',"ls",'blue',True,False)
+# charts['cr2'][('cr2','col1')]=data_column(False,'testx4','line',"ls",'blue',True,False)
+# charts['cr2'][('cr2','col2')]=data_column(False,'testx5','line',"ls",'blue',True,False)
+# charts['cr2'][('cr2','col3')]=data_column(False,'testx6','line',"ls",'blue',True,False)
+# #charts[('ct3','col3')]=data_column(False,'testx','line','blue',ls,True,False)
 
-# #charts={j:chart_dict[j] for j in chart_dict.keys()}
-# print("\n\n")
+#print("\ncd2=",charts.keys())
+fields=chart_elements_dict[column_names[0]]._fields
+print("\ncharts=",chart_names)
+print("\ncolumn names=",column_names)
+print("\nfields=",fields)
+#h=[pd.DataFrame(charts,index=fields) for c in charts.keys()]
 
-# print(charts['cr2'])
-# print("\n",charts['cr2']['col2'])
-# print("\n",charts['cr2']['col2'][2])
-# #print(data_column('col1'))
-# #print("ct=",charts)
-# #report_dict = defaultdict(lambda: defaultdict(dict))
-# chart=[]
-
-# for c in charts.keys():
-#  #   f=chart_elements_dict['col1']   #.keys()
-#   #  g=data_column('name',...)
-#  #   print("c,f",c,f._fields)
-#     new_chart=pd.DataFrame(charts[c],index=chart_elements_dict['col1']._fields).T
-#     print("new chart=\n",c,new_chart)
-#     chart.append(new_chart 
-h=[pd.DataFrame(charts[c],index=chart_elements_dict['col1']._fields) for c in charts.keys()]
-#for c in charts.keys():
-#    pd.DataFrame(charts[c],index=[c,chart_elements_dict['col1']._fields]).stack() 
-#print("h=\n",h)
-
-#chart['ct3']['col3']=('testx',True,'col1','line','blue',True,False)
-#df=df.set_index('market_name', append=True)
-
-#print("h=\n",h[1])
-#h['new']="new"
-#df=pd.MultiIndex.from_frame(pd.concat(h,axis=1),levels=['chart','col'])
-df=pd.concat(h,axis=1)
-#print("df=\n",df)
-#df2=pd.MultiIndex.from_frame(df,names=['chart','2','3'])
-#print("df2=\n",df2)
-
-df.columns.set_names('chart_name', level=0,inplace=True)
-df.columns.set_names('column_name', level=1,inplace=True)
-print("df=\n",df)
-print("charts=\n",charts)
-#df.loc['active',('chart1','col2')]='test'
-
-#print("df=\n",df)
+#df=pd.concat(h,axis=1)
+#h=[pd.DataFrame(charts,index=fields) for c in charts.keys()]
 
 
-#df=df.set_index('market_name', append=True)
-#df=df.set_index('market_name', append=True)
-#print("df2=\n",df2)
-#df2=pd.MultiIndex.from_frame(pd.concat(h,axis=0),names=['chart','2','3'])
-#print("df3=\n",df2)
-#k={h[1]}
-#print(k)
-#print("\n",h[1].loc[:,h[1]['active'==True]])
- #   f=chart_elements_dict['col1']   #.keys()
-  #  g=data_column('name',...)
- #   print("c,f",c,f._fields)
+def convert_dict_to_df(charts,fields):
+    df=pd.concat([pd.DataFrame(charts,index=fields) for c in charts.keys()],axis=1)
+    df.columns.set_names('chart_name', level=0,inplace=True)
+    df.columns.set_names('column_name', level=1,inplace=True)
+    return df.T
+
+
+def plot_df(df,plotp_list):
+  #  fig = plt.figure() # Create matplotlib figure
+   # d=0
+    if isinstance(df, pd.Series):
+        df=df.to_frame()
+    df['date'] = pd.to_datetime(df.index,format="%d/%m/%y",exact=False)
+    df = df.set_index('date', append=True)
+    df.index = df.index.droplevel(0)
+ #   print("plotp_list",plotp_list)
+ #   print("df=\n",df)
+  #  df_prop=pd.DataFrame(plotp)
+  #  print("\ndfp=\n",df_prop)
+  #  print(df_prop.loc[:,'kind'].tolist())
+    ax = plt.gca()   #add_subplot(111) # Create matplotlib axes
+
+    for p in plotp_list:
+     #   print(d,"df=\n",df.iloc[:,d])
+    #    print("p=\n",p)
+        df.plot(ax=ax,kind=p.kind,stacked=p.stacked,use_index=False,legend=False,style=p.style,title=p.title,secondary_y=p.secondary_y)    
+  #      d+=1
+        ax.set_ylabel(p.y_title,fontsize=8)   
+
+
+        ticklabels = ['']*len(df.index)
+         # Every 4th ticklable shows the month and day
+        ticklabels[::8] = [item.strftime('%b') for item in df.index[::8]]
+         # Every 12th ticklabel includes the year
+        ticklabels[10::26] = [item.strftime('\n\n%Y') for item in df.index[10::26]]
+   
+
+ 
+     # #   plt.ylabel("sales (000)",fontsize=7)
+     #  #  plt.xlabel("",fontsize=7)
+
+        plt.gca().xaxis.set_major_formatter(ticker.FixedFormatter(ticklabels))
+     # #   plt.xtick_params(pad=100)
+        plt.gcf().autofmt_xdate()
+
+
+
+
+
+#     ax.xaxis.set_minor_locator(mpl.dates.MonthLocator())
+#     ax.xaxis.set_minor_formatter(mpl.dates.DateFormatter('%b'))
+# #   ax.xtick_params(pad=20)
+#     ax.xaxis.set_major_locator(mpl.dates.YearLocator())
+#     ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%Y'))
+
+     #  ax2.set_ylabel('Price',fontsize=8)
+    ax.set_xlabel("",fontsize=7)
+    plt.gca().legend(fontsize=7)  #,loc='best')
+    #   fig.legend(fontsize=7,loc='best')
+    return
+
+
+def plot_column(df,plotp):
+    #  data_column(active=False, title='Units (000)', kind='line', linestyle='-', colour='blue', stacked=False, second_y=False)
     
-#print("\n\n\nchart=\n",chart)
+  #  print("series=\n",df,plotp.active,"stack len",s.stack_len())
 
-#report_dict[0][1]="chart1"
-# chart 0, column 1
-#report_dict[0][1]=chart_dict[1]
-#report_dict[1][2]=chart_dict[2]
-
-#chart = namedtuple("chart", ["name",chart_elements_dict])
-
-#chart['test1'][1]="yes"
-
-#print("\n",report_dict)
-
-# chart_dict['a']['b']['c'] = 'foo'
-# chart_dict['a']['b']['d'] = 'bar'
-
-# print(chart_dict)
-
-#t['a']['b']['c'] = 'foo'
-#t['a']['b']['d'] = 'bar'
-
-#print(t)
+    if plotp.active:
+        if plotp.stacked:
+            print("add to stack stack length=",s.push_stack(df,plotp))
+        else: 
+            if s.stack_len()>0:
+                plot_df(pd.concat(s.pop_all_dfs_stack(),axis=1),s.pop_all_plotps_stack())
+            else:
+                plot_df(df,[plotp])
+            
+    return    
 
 
-# value is (brand,specialpricecat, productgroup, product,type,name)
-# type is 0 off promo. 1 is on promo, 2 is total of both, 3 is invoiced total
+
+def remove_levels(df,n):
+    df=df.T
+    levels_to_remove = df.index.nlevels-n
+    for _ in range(levels_to_remove):
+        df=df.droplevel(level=0)
+   # print("remove level df=\n",df)
+
+    return df.T
 
 
-#product_type = namedtuple("product_type", ["brandno","customercat", "productgroup","product","type","name"])
-#report_type_dict={0:"dictionary",
-#                        3:"dataframe",
-#                        5:"spreadsheet",
-#                        6:"pivottable",
-#                        8:"chart_filename"}
+
+  
+
+plotp_df=convert_dict_to_df(charts,fields)
+#print("\nplotp_df=\n",plotp_df)
+#print("\ndf.T=\n",df.T)
+#print("charts=\n",charts)
+#print(df.T.index)
+
+#print("\nggg=",charts[('chart1','Units (000)')])
+#print("\nmmm=",charts[('chart1','col1')].stacked)
+#print("\nmm=",charts[('chart1','col1')].style)
+
+#gf=(charts.active==True)
+#pop_whole_stack()
+#print("df=",df)
+
+#print("stack len=",s.stack_len())      
+
+i=0
+for c in charts.keys():
+    
+ #   print("c",c[0],c[1])
+  #  print(remove_levels(df,1))
+    plot_column(remove_levels(df,1).iloc[:,i],charts[c])
+    i+=1
+    plt.show()
+plt.close('all')    
